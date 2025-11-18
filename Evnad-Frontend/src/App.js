6effe622440aceeb6a3705670131146f65dfef81
Pouria <pouriahajigholizadeh@gmail.com>
2025-11-18 19:28:35 +0330

    feat: create layout component and wrap the application with it

--- Evnad-Frontend/src/App.js
+++ Evnad-Frontend/src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/layout/Header.js";
 import "./App.css";
 import HomePage from "./components/layout/home/HomePage.js";
+import Layout from "./components/layout/Index.js";
 
 function App() {
   return (
-    <>
-      <Header />
+    <Layout>
       <HomePage />;
-    </>
+    </Layout>
   );
 }
 
