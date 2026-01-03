4d6c3ad2419dcbb1e81da340231f4e6815c500b1
MAEmamizade <aliem2004.3@gmail.com>
2026-01-03 19:08:14 +0330

    add swagger

--- Evand-Backend/Evand.API/Program.cs
+++ Evand-Backend/Evand.API/Program.cs
@@ -1,15 +1,28 @@
+using Evand.Persistence.DbContextes;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EvandDbContext>(options =>
+{
+    options.UseInMemoryDatabase("EvandDb");
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
