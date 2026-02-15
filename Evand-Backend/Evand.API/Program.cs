fda02a89e8a3915bb7edb22520c6962e323abead
MAEmamizade <aliem2004.3@gmail.com>
2026-02-15 23:16:29 +0330

    add Event service

--- Evand-Backend/Evand.API/Program.cs
+++ Evand-Backend/Evand.API/Program.cs
@@ -1,4 +1,11 @@
+using Evand.Application.DTOs.Event;
+using Evand.Application.Interfaces;
+using Evand.Application.Services;
+using Evand.Domain.Entities;
 using Evand.Persistence.DbContextes;
+using Evand.Persistence.Interfaces;
+using Evand.Persistence.Repositories.Command;
+using Evand.Persistence.Repositories.Query;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +16,12 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IGenericCommandRepository<Event>, GenericCommandRepository<Event>>();
+builder.Services.AddScoped<IGenericQueryRepository<Event>, GenericQueryRepository<Event>>();
+
+builder.Services.AddScoped<IService<Event, EventDto, EventAddOrUpdateDto>, EventService>();
 
 builder.Services.AddDbContext<EvandDbContext>(options =>
 {
@@ -25,30 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// app.UseAuthorization();
+// app.UseAuthentication();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
