2293b6c94f37f1236b77f33c3725b03df5e83b76
MAEmamizade <aliem2004.3@gmail.com>
2026-02-17 15:38:01 +0330

    add EventParticipant repo

---
+++ Evand-Backend/Evand.Persistence/Repositories/Query/EventParticipantQueryRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Evand.Domain.Entities;
+using Evand.Persistence.DbContextes;
+using Evand.Persistence.Interfaces;
+
+namespace Evand.Persistence.Repositories.Query
+{
+    public class EventParticipantQueryRepository(EvandDbContext dbContext) : GenericQueryRepository<EventParticipant>(dbContext), IGenericQueryRepository<EventParticipant>
+    {
+        
+    }
+}
